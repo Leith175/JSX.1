8bcc69ea912d51390c2e18a851e9294e9ab3583d
Leith175 <73946222+Leith175@users.noreply.github.com>
2021-12-12 15:20:55 +0100

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import TodoList from './Todo/TodoList';
+import React, { Component} from 'react';
+
+function App() {
+  return (
+    <TodoList />
+  );
+}
+
+export default App;
